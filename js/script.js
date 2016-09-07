103e39864ca937f97baf49580bc7f37fd4fa6394
KraevaAS <kraevaas@mail.ru>
2016-09-07 21:31:23 +0400

    JS and min

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 (function() {
-  function bindCloseModalEvent(modal, buttons) {
+  function bindCloseModalEvent(modal, buttons, customFn) {
     for (var i = 0; i < buttons.length; i++) {
       var btn = buttons[i];
 
       btn.addEventListener('click', function (e) {
         e.preventDefault();
         modal.classList.remove('open-modal');
+        if (customFn) customFn();
       });
     }
   }
 
-  function bindOpenModalEvent(modal, buttons) {
+  function bindOpenModalEvent(modal, buttons, customFn) {
     for (var i = 0; i < buttons.length; i++) {
       var btn = buttons[i];
 
       btn.addEventListener('click', function (e) {
         e.preventDefault();
         modal.classList.add('open-modal');
+        if (customFn) customFn();
       });
     }
   }
@@ -31,18 +33,6 @@
     var modalOpen = document.querySelectorAll(openClasses);
     bindOpenModalEvent(modal, modalOpen);
   }
-
-  bindModalWindow(
-    '.cta-window',
-    '.cta-window .exit-btn, .cta-window .checkout-btn, .cta-window .continue-btn',
-    '.item .cta-btns .buy-btn'
-  )
-
-  bindModalWindow(
-    '.hidden-form',
-    '.hidden-form .exit-btn, .hidden-form .submit-zone button',
-    '.contacts .feedback-btn'
-  )
 
   function bindServices() {
     function resetActive(elements) {
@@ -68,8 +58,54 @@
         });
       })(i);
     }
-
   }
 
+  function bindMap() {
+    var map = document.querySelector('.map-popup');
+    if (!map) return;
+
+    var contentBottom = document.querySelector('.content-bottom');
+    var mapOpen = document.querySelector('.content-bottom .contacts img');
+    bindOpenModalEvent(map, [mapOpen], function() {
+      contentBottom.classList.add('hidden');
+    })
+
+    var mapClose = document.querySelector('.map-popup .exit-btn');
+    bindCloseModalEvent(map, [mapClose], function() {
+      contentBottom.classList.remove('hidden');
+    })
+  }
+
+  function bindSlider() {
+    var inputs = document.querySelectorAll('.slider > input');
+    if (inputs.length === 0) return;
+
+    var prevButton = document.querySelector('.slider .slider-prevnext .icon-prev');
+    var nextButton = document.querySelector('.slider .slider-prevnext .icon-next');
+
+    prevButton.addEventListener('click', function (e) {
+      e.preventDefault();
+      inputs[0].checked = true;
+    });
+
+    nextButton.addEventListener('click', function (e) {
+      e.preventDefault();
+      inputs[1].checked = true;
+    });
+  }
+
+  bindModalWindow(
+    '.cta-window',
+    '.cta-window .exit-btn, .cta-window .checkout-btn, .cta-window .continue-btn',
+    '.item .cta-btns .buy-btn'
+  )
+
+  bindModalWindow(
+    '.hidden-form',
+    '.hidden-form .exit-btn, .hidden-form .submit-zone button',
+    '.contacts .feedback-btn'
+  )
   bindServices();
+  bindMap();
+  bindSlider();
 })()
